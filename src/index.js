6090c0c4bbd9299fb0a21225196fbd6588c6defc
Sakir Tufan <sakirtufan49@gmail.com>
2021-05-03 16:49:32 +0200

    Create Requests Class

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
-$(()=>{
-  console.log("test");
-})
+import { Request } from "./requests";
+
+$(() => {
+  const form = $("#employee-form");
+  const nameInput = $("#name");
+  const departmentInput = $("#department");
+  const salaryInput = $("#salary");
+  const employeesList = $("#employees");
+  const updateEmployeeButton = $("#update");
+
+  const request = new Request("http://localhost:3000/employees");
+  // request
+  //   .get()
+  //   .then((employees) => {
+  //     console.log(employees);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
+
+  // request
+  //   .post({
+  //     name: "Ümit Efe Tufan",
+  //     department: "Schüler",
+  //     salary: 2000,
+  //   })
+  //   .then((employee) => {
+  //     console.log(employee);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
+
+  // request
+  //   .put(1, {
+  //     name: "Sakir Tufan",
+  //     department: "IT",
+  //     salary: 4100,
+  //   })
+  //   .then((employee) => {
+  //     console.log(employee);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
+
+  // request
+  //   .delete(2)
+  //   .then((message) => {
+  //     console.log(message);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
+
+});
